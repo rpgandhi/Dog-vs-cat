b6438b7cec8429e38587c9b0a2462a710baa4ee6
Rakhee Gandhi and Valia Dempsey <rakhee@>
2017-09-12 16:02:38 -0700

    Add dog and cat reponses and remove all responses upon clicking on any response.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,16 @@ $(document).ready(function() {
   $("img#cat").click(function() {
     $("ul#response-cat").prepend("<li>Meow!</li>");
     $("ul#response-dog").prepend("<li>Bark!</li>");
+    $("ul").children("li").click(function() {
+    $("li").remove();
+    });
   });
 
   $("img#dog").click(function() {
     $("ul#response-cat").prepend("<li>Meow!</li>");
     $("ul#response-dog").prepend("<li>Bark!</li>");
-  });
-
-  $("button#stop").click(function() {
-    $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("ul").children("li").click(function() {
+    $("li").remove();
+    });
   });
 });
